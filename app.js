4a177ecd6efe86adc32bedabe1e44b836938c348
sriram.k <sriram.k@intense.in>
2026-01-21 11:37:41 +0530

    routhes handlers, middle wares and authentication

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("hellow world")
+// app.use("/",(req,res)=>{
+//     res.send("hellow world")
+// })
+// app.use("/2",(req,res)=>{
+//       res.send("this is 2nd rout")
+// })
+
+const{adminAuth} = require("./src/middleware/auth")
+
+app.use("/admin",adminAuth)
+app.get("/admin",(req,res)=>{
+    res.send("hello you are logged in successfully")
 })
-app.use("/2",(req,res)=>{
-      res.send("this is 2nd rout")
+
+
+app.get("/user/:userid/:name",(req,res)=>{
+    console.log(req.params);
+    res.send({firstname:"sriram"})
+});
+
+app.post("/saveUser",(req,res)=>{
+    res.send({firstname:"mittu"})
 })
 
 app.listen(9999,()=>{
